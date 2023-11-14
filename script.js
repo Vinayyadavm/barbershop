7841143d7bc69a34377b1cc645b6f45e3e9f4056
Vinay Yadav <123170049+Vinayyadavm@users.noreply.github.com>
2023-11-14 03:44:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var menubtn = document.getElementById('menubtn');
+var navigation = document.getElementById('navigation');
+var menu = document.getElementById('menu');
+
+navigation.style.right = '-200px';
+
+menubtn.onclick = function () {
+    if (navigation.style.right == '-200px') {
+        navigation.style.right = '0';
+        menu.src = "images/close.png";
+    } else {
+        navigation.style.right = '-200px';
+        menu.src = "images/menu.png";
+    }
+}
